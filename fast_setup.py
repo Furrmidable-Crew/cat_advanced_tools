e2a2fe250836c9c2d90c67c68b8cb97cb58c385d
Nicola <nicorb932@hotmail.com>
2023-10-26 11:00:32 +0200

    fix import settings

--- fast_setup.py
+++ fast_setup.py
@@ -1,12 +1,22 @@
 import json
+import os.path
+
 from cat.mad_hatter.decorators import tool, hook
 
-with open("cat/plugins/cat_advanced_tools/settings.json", "r") as json_file:
-    settings = json.load(json_file)
+
+def get_settings():
+    if os.path.isfile("cat/plugins/cat_advanced_tools/settings.json"):
+        with open("cat/plugins/cat_advanced_tools/settings.json", "r") as json_file:
+            settings = json.load(json_file)
+    else:
+        with open("cat/plugins/cat_advanced_tools_main/settings.json", "r") as json_file:
+            settings = json.load(json_file)
+    return settings
 
 
 @hook
 def agent_prompt_prefix(prefix, cat):
+    settings = get_settings()
     prefix = settings["prompt_prefix"]
 
     return prefix
@@ -14,6 +24,7 @@ def agent_prompt_prefix(prefix, cat):
 
 @hook
 def before_cat_recalls_episodic_memories(default_episodic_recall_config, cat):
+    settings = get_settings()
     default_episodic_recall_config["k"] = settings["episodic_memory_k"]
     default_episodic_recall_config["threshold"] = settings["episodic_memory_threshold"]
 
@@ -22,6 +33,7 @@ def before_cat_recalls_episodic_memories(default_episodic_recall_config, cat):
 
 @hook
 def before_cat_recalls_declarative_memories(default_declarative_recall_config, cat):
+    settings = get_settings()
     default_declarative_recall_config["k"] = settings["declarative_memory_k"]
     default_declarative_recall_config["threshold"] = settings["declarative_memory_threshold"]
 
@@ -30,6 +42,7 @@ def before_cat_recalls_declarative_memories(default_declarative_recall_config, c
 
 @hook
 def before_cat_recalls_procedural_memories(default_procedural_recall_config, cat):
+    settings = get_settings()
     default_procedural_recall_config["k"] = settings["procedural_memory_k"]
     default_procedural_recall_config["threshold"] = settings["procedural_memory_threshold"]
 
