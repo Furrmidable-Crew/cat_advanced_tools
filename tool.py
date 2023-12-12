715ec596983d8af050ad7d9c254e0918acb7be04
Nicola <nicorb932@hotmail.com>
2023-12-12 20:08:13 +0100

    remove tool

--- tool.py
+++
@@ -1,9 +0,0 @@
-from cat.mad_hatter.decorators import tool
-
-
-@tool(return_direct=True)
-def show_working_memory(none, cat):
-    """Useful to display the content of the working memory. Input is None."""
-
-    # print(cat.working_memory)
-    return f"{cat.working_memory.keys()}"
